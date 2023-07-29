45efaaeb6ffbebff9aa68af32ed950f51f433922
ziyabnajeeb <40266679+ziyabnajeeb@users.noreply.github.com>
2023-07-29 06:49:02 +0500

    how lendo works section added

--- src/components/LandinPage/HowLendoWorks/HowLendoWorks.jsx
+++ src/components/LandinPage/HowLendoWorks/HowLendoWorks.jsx
@@ -1,8 +1,20 @@
+import { Col, Container, Row } from 'react-bootstrap';
+
 import './HowLendoWorks.scss'
 
 const HowLendoWorks = () => {
   return (
-    <div>HowLendoWorks</div>
+    <Container as='section' className='py-5 my-5 lendo__works'>
+    <Row>
+      <Col lg={9} className='mx-auto'>
+        <div className='lendo__works-bg'>
+          <div className='d-flex justify-content-center'>
+            <p className='fw-lighter'>Smartly. Conveniently. Efficiently</p>
+          </div>
+        </div>
+      </Col>
+    </Row>
+  </Container>
   )
 }
 
