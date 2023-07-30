cc4e5f48bf6b30383013c2933a3e68a04a66363c
ziyabnajeeb <40266679+ziyabnajeeb@users.noreply.github.com>
2023-07-30 17:15:12 +0500

    fixes done

--- src/components/LandinPage/NewToInvesting/NewToInvesting.jsx
+++ src/components/LandinPage/NewToInvesting/NewToInvesting.jsx
@@ -14,31 +14,31 @@ const NewToInvesting = () => {
         <Col lg={10} className='mx-auto'>
           <Tabs id='lendo__new-to-investing-section' activeKey={key} onSelect={(k) => setKey(k)} className='mb-3 justify-content-center lendo__new-to-investing-section'>
             <Tab eventKey='individuals' title='For Individuals'>
-              <div className='d-flex flex-column justify-content-center'>
-                <div className='lendo__new-to-investing-section-content-header py-5 text-center'>
+              <div className='lendo__new-to-investing-section-content-wrapper d-flex flex-column justify-content-center'>
+                <div className='lendo__new-to-investing-section-content-header text-center'>
                   <p class='fw-lighter mb-0'>Smartly. Conveniently. Efficiently</p>
                   <h2 class='fw-bolder text-white'>Startup, medium, or large?</h2>
                 </div>
 
-                <Row className='justify-content-center text-center py-5'>
+                <Row className='lendo__new-to-investing-section-content-list justify-content-center text-center'>
                   <Col lg={4}>
                     <div className='w-75 mx-auto'>
                       <Image src={iconSmartly} className='icon-smartly' />
-                      <h4 className='fw-bold text-white py-4'>Smartly</h4>
+                      <h4 className='fw-bold text-white pt-5 pb-3'>Smartly</h4>
                       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
                     </div>
                   </Col>
                   <Col lg={4}>
                     <div className='w-75 mx-auto'>
                       <Image src={iconConveniently} className='icon-smartly' />
-                      <h4 className='fw-bold text-white py-4'>Conveniently</h4>
+                      <h4 className='fw-bold text-white pt-5 pb-3'>Conveniently</h4>
                       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
                     </div>
                   </Col>
                   <Col lg={4}>
                     <div className='w-75 mx-auto'>
                       <Image src={iconEfficiently} className='icon-smartly' />
-                      <h4 className='fw-bold text-white py-4'>Efficiently</h4>
+                      <h4 className='fw-bold text-white pt-5 pb-3'>Efficiently</h4>
                       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
                     </div>
                   </Col>
@@ -47,7 +47,7 @@ const NewToInvesting = () => {
             </Tab>
 
             <Tab eventKey='business' title='For Business'>
-              <div className='d-flex flex-column justify-content-center'>
+              <div className='lendo__new-to-investing-section-content-wrapper d-flex flex-column justify-content-center'>
                 <div className='lendo__new-to-investing-section-content-header py-5 text-center'>
                   <p class='fw-lighter mb-0'>Smartly. Conveniently. Efficiently</p>
                   <h2 class='fw-bolder text-white'>For Business Header</h2>
