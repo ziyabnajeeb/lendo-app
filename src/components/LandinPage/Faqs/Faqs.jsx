6fffed9b688ce0d67870f967050f15a96f6cccb1
ziyabnajeeb <40266679+ziyabnajeeb@users.noreply.github.com>
2023-07-30 22:52:02 +0500

    faqs section completed

--- src/components/LandinPage/Faqs/Faqs.jsx
+++ src/components/LandinPage/Faqs/Faqs.jsx
@@ -1,9 +1,67 @@
-import './Faqs.scss'
+import { Accordion, Col, Container, Row } from 'react-bootstrap';
+
+import './Faqs.scss';
 
 const Faqs = () => {
   return (
-    <div>Faqs</div>
-  )
-}
+    <Container className='lendo__faqs'>
+      <Row>
+        <Col lg={12}>
+          <div className='lendo__faqs-header w-50'>
+            <h2 className='fw-bolder'>FAQs</h2>
+            <p>
+              Get prompt responses from a friendly, professional and knowledgable support team. Have more questions? <a href='#/'>Visit our FAQs page</a>
+            </p>
+          </div>
 
-export default Faqs
+          <div className='lendo__faqs-content'>
+            <Accordion>
+              <Accordion.Item eventKey='0'>
+                <Accordion.Header>What is Lendo?</Accordion.Header>
+                <Accordion.Body>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                  sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey='1'>
+                <Accordion.Header>Is Lendo a Saudi company and registered in Saudi?</Accordion.Header>
+                <Accordion.Body>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                  sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey='2'>
+                <Accordion.Header>What is Crowd-funding/ Crowd-lending?</Accordion.Header>
+                <Accordion.Body>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                  sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey='3'>
+                <Accordion.Header>Who can invest in Lendo opportunities?</Accordion.Header>
+                <Accordion.Body>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                  sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey='4'>
+                <Accordion.Header>Who can I contact if I have a complaint?</Accordion.Header>
+                <Accordion.Body>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                  sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+          </div>
+
+          <div className="lendo__faqs-more text-center">
+            <h5 className='fw-bold'>Got more questions?</h5>
+            <p>Please <a href='#/'>Contact us</a> and we will answer your questions</p>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Faqs;
